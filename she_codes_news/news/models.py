fd23a7217b3ce3413172498b80f8c82ce63497e4
Isobel Young <isobel.hy@gmail.com>
2020-08-25 17:37:28 +1000

    Additions for Part 1 of Project
    Ordered news items by date and image URL functionality to form.

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -7,5 +7,6 @@ class NewsStory(models.Model):
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
+    img_url = models.CharField(max_length=200, default='https://picsum.photos/600')
 
 # add categories, authors
--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -9,12 +9,12 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         '''Return all news stories.'''
-        return NewsStory.objects.all()
+        return NewsStory.objects.all().order_by('pub_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all()[:4]
-        context['all_stories'] = NewsStory.objects.all()
+        context['latest_stories'] = NewsStory.objects.order_by('-pub_date').all()[:4] #  added order_by to sort stories by date
+        context['all_stories'] = NewsStory.objects.all().order_by('-pub_date') #  added order_by to sort stories by date
         return context
 
 
